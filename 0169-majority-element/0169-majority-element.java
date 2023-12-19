dbf4c944b4854485c715bf1fbad2508ef8b5d282
NavyaTrilok <dtriloknavya@gmail.com>
2023-12-19 13:57:57 -0700

    Time: 1 ms (99.85%), Space: 49.1 MB (13.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,21 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int majority = nums[0];
+        int votes = 1;
+        
+        for(int i = 1; i < nums.length; i++){
+            if(votes == 0){
+                votes++;
+                majority = nums[i];
+                
+            }
+            else if(majority == nums[i]){
+                votes++;
+            }else{
+                votes--;
+            }
+        }
+        
+        return majority;
     }
 }
