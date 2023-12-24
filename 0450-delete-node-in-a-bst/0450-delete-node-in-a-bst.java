8d1885e0f4eba656b2aef87a97ea9af6200af7db
NavyaTrilok <dtriloknavya@gmail.com>
2023-12-23 23:55:56 -0700

    Time: 0 ms (100.00%), Space: 45.9 MB (8.07%) - LeetHub

---
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null){
+            return null;
+        }
+        if(key < root.val){
+            root.left = deleteNode(root.left,key);
+        }else if(key > root.val){
+            root.right = deleteNode(root.right,key);
+        }else if(key == root.val) {
+            //work
+            if(root.left!=null && root.right!=null){
+                int lmax = max(root.left);
+                root.val = lmax;
+                root.left = deleteNode(root.left,lmax);
+                return root;
+            }else if(root.left != null){
+                return root.left;
+            }else if(root.right != null){
+                return root.right;
+            }else{
+                return null;
+            }
+            
+        }
+        
+        return root;
+
+    }
+    
+    public static int max(TreeNode node){
+        if(node.right != null){
+            return max(node.right);
+        }else{
+            return node.val;
+        }
+        
+    }
+}
