d201b446963ca0301c985f97cccb5778e64bf30f
NavyaTrilok <dtriloknavya@gmail.com>
2023-12-27 17:00:29 -0700

    Time: 0 ms (100.00%), Space: 46.1 MB (5.38%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -15,40 +15,42 @@
  */
 class Solution {
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null){
-            return null;
-        }
+        if(root == null) return null;
+        
         if(key < root.val){
             root.left = deleteNode(root.left,key);
-        }else if(key > root.val){
+        } else if (key > root.val){
             root.right = deleteNode(root.right,key);
-        }else if(key == root.val) {
+        }else if(key == root.val){
             //work
             if(root.left!=null && root.right!=null){
+            
                 int lmax = max(root.left);
+                
                 root.val = lmax;
+                
                 root.left = deleteNode(root.left,lmax);
+                
                 return root;
-            }else if(root.left != null){
+                
+            }else if(root.left!=null){
                 return root.left;
-            }else if(root.right != null){
+            }else if(root.right!=null){
                 return root.right;
-            }else{
+            }else if(root.left == null && root.right == null){
                 return null;
             }
-            
         }
         
         return root;
-
+        
     }
     
     public static int max(TreeNode node){
-        if(node.right != null){
-            return max(node.right);
-        }else{
-            return node.val;
-        }
-        
+       if(node.right!=null){
+           return max(node.right);
+       }else{
+           return node.val;
+       }
     }
 }
