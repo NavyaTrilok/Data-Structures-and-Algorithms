d61dbcce5d5232d5c197bc6e17dca21e84bb7d35
NavyaTrilok <dtriloknavya@gmail.com>
2024-04-18 15:13:17 -0700

    Time: 10 ms (81.45%), Space: 11.7 MB (49.71%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,4 +1,16 @@
 class Solution(object):
     def truncateSentence(self, s, k):
-        return " ".join(s.split()[:k])
+        words = s.split(" ")
+        count = 0
+        res = ""
+        for word in words:
+            count += 1
+            if count == k:
+                res +=word
+            else:
+                res += word+" "
+            if count == k:
+                return res
+                
+            
         
