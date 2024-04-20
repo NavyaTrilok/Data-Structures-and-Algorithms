9d7408eb56082f539781bf7d7838313f484b6cf0
NavyaTrilok <dtriloknavya@gmail.com>
2024-04-19 17:41:46 -0700

    Time: 13 ms (66.29%), Space: 11.6 MB (85.65%) - LeetHub

--- 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.py
@@ -1,15 +1,14 @@
 class Solution(object):
     def balancedStringSplit(self, s):
-        res = 0;
-        count = 0;
+        count = 0
+        res = 0
         
         for i in s:
-            if i == 'L':
-                count -= 1
-            if i == 'R':
-                count += 1
-            if count == 0:
-                res += 1
+            if i=='L':
+                count-=1
+            if i =='R':
+                count+=1
+            if count==0:
+                res+=1
         return res
-        
         
