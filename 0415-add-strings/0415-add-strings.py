598b43146decc9ffecc38c6b8fcb69f4a73e38a0
NavyaTrilok <dtriloknavya@gmail.com>
2024-04-22 13:18:00 -0700

    Time: 27 ms (32.43%), Space: 11.7 MB (88.56%) - LeetHub

--- 0415-add-strings/0415-add-strings.py
+++ 0415-add-strings/0415-add-strings.py
@@ -1,16 +1,21 @@
 class Solution(object):
     def addStrings(self, num1, num2):
-        num1, num2 = num1[::-1], num2[::-1]
-        num3 = ''
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        l1 = len(num1)
+        l2 = len(num2)
+        num3 = ""
+        l3 = max(l1,l2)
         carry = i = 0
-        l1, l2 = len(num1), len(num2)
-        l3 = max(l1, l2)
         while i < l3 or carry:
-            dig1  = int(num1[i]) if i < l1 else 0
-            dig2  = int(num2[i]) if i < l2 else 0
-            dig3  = (dig1 + dig2 + carry) % 10
-            carry = (dig1 + dig2 + carry) // 10
+            dig1 = int(num1[i]) if i < l1 else 0
+            dig2 = int(num2[i]) if i < l2 else 0
+            dig3 = (dig1 + dig2 + carry)%10
+            carry = (dig1 + dig2 + carry)//10
             num3 += str(dig3)
-            i    += 1
+            i += 1
         return num3[::-1]
+        
+        
+        
         
