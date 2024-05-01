bf2caee3b9ed3f341d881d492d0c1a8a6dd6a2ca
NavyaTrilok <dtriloknavya@gmail.com>
2024-05-01 08:17:51 -0700

    Time: 89 ms (16.94%), Space: 11.8 MB (92.27%) - LeetHub

---
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def buildArray(self, nums):
+        ans = [None] * len(nums)
+        for i in range(len(nums)):
+            ans[i] = nums[nums[i]]
+        return ans        
