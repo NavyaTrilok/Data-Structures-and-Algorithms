5783be92bbb4285e2801860448a5ca72633dca18
NavyaTrilok <dtriloknavya@gmail.com>
2024-05-01 09:35:47 -0700

    Time: 18 ms (87.88%), Space: 12 MB (17.98%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.py
@@ -1,13 +1,12 @@
 class Solution(object):
     def mostWordsFound(self, sentences):
-        
         length = 0
+        
         for sentence in sentences:
             split_sentence = sentence.split()
+            
             if length < len(split_sentence):
                 length = len(split_sentence)
-                
+        
         return length
-    
-
         
