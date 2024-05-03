f8c446aec134d48bdf97f1d12074c2fba3cd94d1
NavyaTrilok <dtriloknavya@gmail.com>
2024-05-02 20:56:04 -0700

    Time: 12 ms (71.42%), Space: 11.6 MB (69.20%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.py
@@ -2,8 +2,9 @@ class Solution(object):
     def reversePrefix(self, word, ch):
         if ch not in word:
             return word
-        res = word.index(ch)
+        for x in word:
+            if x == ch:
+                res = word.index(ch)
         ans = word[:res+1][::-1]+word[res+1:]
         return ans
-        #stringname[stringlength::-1] 
-        #word[idx+1:]
+        
