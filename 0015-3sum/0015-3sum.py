348a75faaeb2fb62f4ccdfc71ac0abfb324f058e
NavyaTrilok <dtriloknavya@gmail.com>
2024-06-28 21:11:56 -0700

    Time: 2024 ms (29.87%), Space: 14.6 MB (96.90%) - LeetHub

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,30 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        set_ans = set()
+        nums.sort()
+        
+        for i in range(len(nums)-2):
+            left = i + 1
+            right = len(nums)-1
+            
+            while left < right:
+                sum = nums[i]+nums[left]+nums[right]
+                if sum == 0:
+                    set_ans.add((nums[i], nums[left], nums[right]))
+                    right -= 1
+                    left += 1
+            
+                elif sum > 0:
+                    right -= 1
+                
+                else:
+                    left += 1
+                
+        return list(set_ans)
+            
+                
+        
