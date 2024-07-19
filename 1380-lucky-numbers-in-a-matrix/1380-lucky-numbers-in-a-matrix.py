4aa4c42b162f428485955321dd29c1506ee69273
NavyaTrilok <dtriloknavya@gmail.com>
2024-07-19 15:06:42 -0700

    Time: 97 ms (33.60%), Space: 11.9 MB (21.60%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.py
@@ -4,3 +4,4 @@ class Solution(object):
         maxcol = {max(x) for x in zip(*matrix)}
         return list(minrow & maxcol)
         
+        #zip(*matrix) is a Python idiom to transpose the matrix. It converts rows to columns and #vice versa.
