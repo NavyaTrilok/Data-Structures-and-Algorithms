4e58f2dea3f6fabe6c94e4eaeecaa67ac0152116
NavyaTrilok <dtriloknavya@gmail.com>
2024-08-16 14:15:52 -0700

    Time: 8 ms (43.66%), Space: 44.5 MB (29.23%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,18 +1,19 @@
 class Solution {
-    Map<String, Boolean> map = new HashMap<>(); 
-    public boolean wordBreak(String s, List<String> worddict) {
-        if(worddict.contains(s))
+    Map<String, Boolean> map = new HashMap<>();
+    public boolean wordBreak(String s, List<String> wordDict) {
+        if(wordDict.contains(s))
             return true;
-        
+            
         if(map.containsKey(s))
             return map.get(s);
-        
+            
+            
         for(int i = 1; i <= s.length(); i++){
-            String left = s.substring(0, i);
-            if(worddict.contains(left) && wordBreak(s.substring(i),worddict)){
+            if(wordDict.contains(s.substring(0,i)) && wordBreak(s.substring(i),wordDict)){
                 map.put(s,true);
                 return true;
             }
+                
         }
         map.put(s,false);
         return false;
