5ae0bcc646f669ffd4fa8f4162a2514677bdfca9
NavyaTrilok <dtriloknavya@gmail.com>
2024-09-19 09:10:03 -0700

    Time: 2 ms (44.02%), Space: 44.4 MB (50.77%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,30 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        
         List<List<Integer>> ans = new ArrayList<>();
         
         countpermutations(nums,ans,new ArrayList<>());
         
         return ans;
+        
     }
     
-    public void countpermutations(int[] nums, List<List<Integer>> ans, List<Integer> list){
+    public void countpermutations(int[] nums, List<List<Integer>> ans, List<Integer> list ){
+        
         if(list.size()==nums.length){
             ans.add(new ArrayList<>(list));
             return;
         }
+        
         for(int num:nums){
             if(list.contains(num)){
                 continue;
+            }else{
+                list.add(num);
+                countpermutations(nums,ans,list);
+                list.remove(list.size()-1);
             }
-            list.add(num);
-            countpermutations(nums,ans,list);
-            list.remove(list.size()-1);
         }
+        
     }
 }
