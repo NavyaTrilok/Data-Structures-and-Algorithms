9ff9359b7eb7e846753c7239417e2f4d43b6cdf7
NavyaTrilok <dtriloknavya@gmail.com>
2024-09-29 15:01:29 -0700

    Time: 5 ms (99.87%), Space: 46.1 MB (13.18%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,24 +1,24 @@
 class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {     
+    public boolean searchMatrix(int[][] matrix, int target) {
         
         int row = 0;
-        int col = matrix[0].length - 1;
+        int col = matrix[0].length-1;
         
         if(row==col){
             if(matrix[row][col]==target)
                 return true;
         }
         
-        while( col >= 0 && row <= matrix.length-1){
+        while(row <= matrix.length-1 && col >= 0){
             if(matrix[row][col] == target)
                 return true;
-                
+            
             if(matrix[row][col] < target)
                 row++;
             else
                 col--;
-                
         }
+        
         return false;
         
     }
