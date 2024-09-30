43828e68d57ae956d1b7d2234d995527a542c545
NavyaTrilok <dtriloknavya@gmail.com>
2024-09-30 07:18:00 -0700

    Time: 476 ms (64.49%), Space: 22.2 MB (77.68%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -5,20 +5,12 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        """"
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if nums[i] == nums[j] and abs(i - j) <= k and i!=j:
-                    return True
-        return False
-        
-        This solution gives TLE
-        """
-        
         
         index_map = {}
+        
         for i, num in enumerate(nums):
             if num in index_map and abs(i - index_map[num]) <= k:
                 return True
             index_map[num] = i
         return False
+        
