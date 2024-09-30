fa596ec5dc32b03aacabb5bf1171b266f70e843a
NavyaTrilok <dtriloknavya@gmail.com>
2024-09-30 09:53:22 -0700

    Time: 6 ms (29.65%), Space: 44.6 MB (22.62%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,21 +1,27 @@
 class Solution {
     public int thirdMax(int[] nums) {
+        
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         Set<Integer> set = new HashSet<>();
-        for (int i : nums) {
-            if (!set.contains(i)) {
-                pq.offer(i);
+        
+        for (int i : nums){
+            if(!set.contains(i)){
+                pq.add(i);
                 set.add(i);
-                if (pq.size() > 3) {
-                    set.remove(pq.poll());
+                
+                if(pq.size() > 3){
+                   set.remove(pq.poll());
                 }
             }
         }
-        if (pq.size() < 3) {
-            if (pq.size() > 1) {
+        
+        if(pq.size()<3){
+            if(pq.size()>1){
                 pq.poll();
             }
         }
+        
         return pq.peek();
+        
     }
 }
