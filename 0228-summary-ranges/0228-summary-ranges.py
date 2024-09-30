c8aa3b329af30e33d6e906ca15618aadcdd9d579
NavyaTrilok <dtriloknavya@gmail.com>
2024-09-30 07:55:05 -0700

    Time: 15 ms (47.03%), Space: 11.6 MB (83.92%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.py
+++ 0228-summary-ranges/0228-summary-ranges.py
@@ -1,5 +1,23 @@
-class Solution:
-    def summaryRanges(self, nums: List[int]) -> List[str]:
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        """"
+        ranges = []
+        
+        for n in nums:
+            if ranges and ranges[-1][1] == n - 1:
+                ranges[-1][1] = n
+            else:
+                ranges.append([n,n])
+        
+        #return  [f'{x}->{y}'  if x ! = y else f'{x}' for x, y in ranges]
+        return  [f'{x}->{y}'  if x !=  y else f'{x}' for x, y in ranges]
+        
+        """
+        
         ranges = [] # [start, end] or [x, y]
         for n in nums:
             if ranges and ranges[-1][1] == n-1:
@@ -7,5 +25,6 @@ class Solution:
             else:
                 ranges.append([n, n])
 
-        return [f'{x}->{y}' if x != y else f'{x}' for x, y in ranges]
+        #return [f'{x}->{y}' if x != y else f'{x}' for x, y in ranges]
+        return ['{}->{}'.format(x, y) if x != y else '{}'.format(x) for x, y in ranges]
         
