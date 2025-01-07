ef08f78f0c2c8fe2251b6494a4c14bc3dee29e43
Navya Trilok <dtriloknavya@gmail.com>
2025-01-07 07:37:05 -0600

    Time: 3 ms (50.53%), Space: 12.4 MB (36.84%) - LeetHub

---
+++ 0857-positions-of-large-groups/0857-positions-of-large-groups.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def largeGroupPositions(self, s):
+        """
+        :type s: str
+        :rtype: List[List[int]]
+        """
+
+        ans = []
+        i = 0
+        for j in range(len(s)):
+            if j == len(s)-1 or s[j]!=s[j+1]:
+                if j - i + 1 >= 3:
+                    ans.append([i,j])
+                i = j + 1
+        return ans
+        
