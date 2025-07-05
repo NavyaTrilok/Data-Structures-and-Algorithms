7d4d8e42b2c53ee6e745a456c1ee813f9208f9be
Navya <dtriloknavya@gmail.com>
2025-07-05 12:20:22 -0500

    Time: 8 ms (6.24%), Space: 44.9 MB (9.6%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,25 +14,32 @@
  * }
  */
 class Solution {
-    
-    String preorderTraversal(TreeNode node){
-        if(node == null){
+
+    public String preorder(TreeNode node){
+
+        if(node == null)
             return "null";
-        }
-        
+
         StringBuilder sb = new StringBuilder("^");
+
         sb.append(node.val);
-        sb.append(preorderTraversal(node.left));
-        sb.append(preorderTraversal(node.right));
-        
+        sb.append(preorder(node.left));
+        sb.append(preorder(node.right));
+
         return sb.toString();
+        
     }
+
+
+
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        
-        String preorderroot = preorderTraversal(root);
-        String preorderSubroot = preorderTraversal(subRoot);
-        
-        return preorderroot.contains(preorderSubroot);
-        
+        if(root == null || subRoot == null)
+            return false;
+
+        String preordertraversal = preorder(root);
+        String preordersubroottraversal = preorder(subRoot);
+
+        return preordertraversal.contains(preordersubroottraversal);
+ 
     }
 }
