2b07b9e273ce08605f51bf60c939fafc65fe9301
Navya <dtriloknavya@gmail.com>
2025-07-13 11:06:23 -0500

    Time: 3 ms (100%), Space: 54.1 MB (74.72%) - LeetHub

---
+++ 3931-process-string-with-special-operations-i/3931-process-string-with-special-operations-i.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String processStr(String s) {
+
+        StringBuilder br = new StringBuilder();
+
+        for(int i=0; i < s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '*'){
+                if(br.length() > 0){
+                    br.deleteCharAt(br.length()-1);
+                }
+            }else if(ch == '#'){
+                br.append(br.toString());
+            }else if(ch == '%'){
+                br = new StringBuilder(br.reverse());
+            }else{
+                br.append(ch);
+            }
+
+        }
+
+        return br.toString();
+        
+    }
+}
