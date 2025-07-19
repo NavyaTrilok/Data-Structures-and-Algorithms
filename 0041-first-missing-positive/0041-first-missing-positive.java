865494b4f216bd535db9f9fca9138d6005dedc29
Navya <dtriloknavya@gmail.com>
2025-07-19 10:29:31 -0500

    Time: 1 ms (100%), Space: 57.6 MB (33.95%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,31 +1,31 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        //step1: Mark elements which are out of range and manage presence of one
-        boolean one = false;
+
+        Boolean one = false;
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == 1){
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == 1)
                 one = true;
-            }
-            if( nums[i] < 1 || n < nums[i]){
+
+            if(nums[i] < 1 || nums[i] > n)
                 nums[i] = 1;
-            }
         }
+
         if(one == false) return 1;
 
-        //step2: Map elements with index
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < nums.length; i++){
             int idx = Math.abs(nums[i]);
+
             nums[idx - 1] = -Math.abs(nums[idx - 1]);
         }
 
-        //step3: Find out missing integer
-        for(int i = 0; i < n; i++){
-            //if element is positive that mean idx+1 is missing int the array
-            if(nums[i] > 0) return i + 1;
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] > 0)
+                return i + 1;
         }
 
-        return n + 1;
+    return n+1;
         
     }
 }
