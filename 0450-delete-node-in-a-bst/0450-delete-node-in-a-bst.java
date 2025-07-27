8a5e0c5611fdfe54b7b6c04f3195b0245d2aac35
Navya <dtriloknavya@gmail.com>
2025-07-27 16:26:13 -0500

    Time: 0 ms (100%), Space: 46.4 MB (13.96%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.java
@@ -14,42 +14,58 @@
  * }
  */
 class Solution {
-    public TreeNode deleteNode(TreeNode root, int key) {
-        if(root == null) return null;
+    
+      public int min(TreeNode root) 
+    {
+        // write your code here
+        while(root.left!=null)
+        {
+            root=root.left;
+        }
+
+        return root.val;
+    }
+
+    
+    public TreeNode deleteNode(TreeNode root, int key) 
+    {
+        if(root==null) return null;
         
-        if(key < root.val){
-            root.left = deleteNode(root.left,key);
-        } else if (key > root.val){
-            root.right = deleteNode(root.right,key);
-        }else if(key == root.val){
-            //work
-            if(root.left!=null && root.right!=null){
-            
-                int lmax = max(root.left);
-                
-                root.val = lmax;
-                
-                root.left = deleteNode(root.left,lmax);
-                
-                return root;
-                
-            }else if(root.left!=null){
-                return root.left;
-            }else if(root.right!=null){
-                return root.right;
-            }else if(root.left == null && root.right == null){
+        
+        if(root.val==key)
+        {
+            if(root.right==null && root.left==null)
                 return null;
+            if(root.right==null)
+            {
+                return root.left;
             }
+            if(root.left==null)
+            {
+                return root.right;
+            }
+            
+            int right_min=min(root.right);
+            
+            root.val=right_min;
+            root.right=deleteNode(root.right,right_min);
+            
+            return root;
+        }
+
+        if(root.val<key)
+        {
+            root.right=deleteNode(root.right,key);
+        }
+        else
+        {
+            root.left=deleteNode(root.left,key);
         }
         
         return root;
-        
+     
     }
+  
     
-    public static int max(TreeNode node){
-        while(node.right!=null){
-            node = node.right;
-        }
-        return node.val;
-    }
+      
 }
