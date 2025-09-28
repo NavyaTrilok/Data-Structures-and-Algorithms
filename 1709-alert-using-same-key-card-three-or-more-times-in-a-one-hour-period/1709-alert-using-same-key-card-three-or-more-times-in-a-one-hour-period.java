e865b3a259030e45db2f950ee6bab506f600492b
Navya <dtriloknavya@gmail.com>
2025-09-28 07:49:04 -0500

    Time: 90 ms (35.21%), Space: 65.2 MB (64.3%) - LeetHub

--- 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
+++ 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.java
@@ -1,37 +1,42 @@
 class Solution {
     public List<String> alertNames(String[] keyName, String[] keyTime) {
-        Map<String, List<Integer>> map = new HashMap<>();
-        List<String> res = new ArrayList<>();
+        Map<String, List<Integer>> hm = new HashMap<>();
+        ArrayList<String> res = new ArrayList<>();
 
-        // Build name -> list of minutes
-        for (int i = 0; i < keyName.length; i++) {
+        for(int i = 0; i < keyName.length; i++){
             String k = keyName[i];
 
-            // Option 2: explicit if
-            if (!map.containsKey(k)) {
-                map.put(k, new ArrayList<>());
+            if(!hm.containsKey(k)){
+                hm.put(k,new ArrayList<>());
             }
-            map.get(k).add(getTime(keyTime[i]));
+            hm.get(k).add(getTime(keyTime[i]));
         }
 
-        // For each person, sort times and check any 3 within 60 minutes
-        for (String k : map.keySet()) {
-            List<Integer> times = map.get(k);
+        for(String k : hm.keySet()){
+            List<Integer> times = hm.get(k);
+
             Collections.sort(times);
-            for (int i = 2; i < times.size(); i++) {
-                if (times.get(i) - times.get(i - 2) <= 60) {
+            
+            for(int i = 2; i < times.size(); i++){
+
+                if(times.get(i) - times.get(i-2) <= 60){
                     res.add(k);
                     break;
                 }
+
             }
+        
         }
 
         Collections.sort(res);
         return res;
+        
     }
 
-    private int getTime(String s) {
-        String[] p = s.split(":");
-        return Integer.parseInt(p[0]) * 60 + Integer.parseInt(p[1]);
+    public int getTime(String KeyTime){
+        String[] p = KeyTime.split(":");
+        int mins = Integer.parseInt(p[0])*60+Integer.parseInt(p[1]);
+        return mins;
+
     }
 }
