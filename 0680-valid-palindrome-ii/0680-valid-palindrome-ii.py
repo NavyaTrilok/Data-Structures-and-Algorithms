4f459470973a74cb0cc4bd81854fbd9f17f32349
Navya <dtriloknavya@gmail.com>
2025-10-25 15:15:03 -0500

    Time: 84 ms (34.48%), Space: 12.6 MB (89.57%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,21 +1,27 @@
 class Solution(object):
     def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         i = 0
         j = len(s) - 1
+
         while i < j:
             if s[i] == s[j]:
-                i+=1
-                j-=1
+                i += 1
+                j -= 1
             else:
-                return self.ispalindrome(s, i+1, j) or self.ispalindrome(s, i, j-1)
+                return self.isPalindrome(s, i+1, j) or self.isPalindrome(s, i, j-1)
         return True
-    
-    def ispalindrome(self, s, i , j):
+
+    def isPalindrome(self, s, i, j):
         while i < j:
             if s[i] == s[j]:
-                i+=1
-                j-=1
+                i += 1
+                j -= 1
             else:
                 return False
         return True
+
         
