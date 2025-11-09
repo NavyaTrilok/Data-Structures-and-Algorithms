1b82e532d283a48b5a1322d640e86e8bfbebca0c
Navya <dtriloknavya@gmail.com>
2025-11-08 18:13:33 -0600

    Time: 7 ms (60.18%), Space: 13.7 MB (10.32%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -4,12 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-        
-        j = 0  # Pointer for non-zero elements
-        
-        # Move all non-zero elements to the front
-        for i in range(len(nums)):
-            if nums[i] != 0:
-                nums[i], nums[j] = nums[j], nums[i]
+        i = 0
+        j = 0
+        for i in range(0, len(nums)):
+            if nums[i]!=0:
+                temp = nums[i]
+                nums[i] = nums[j]
+                nums[j] = temp
+                i += 1
                 j += 1
-        
+            else:
+                i += 1
+
+        return nums
